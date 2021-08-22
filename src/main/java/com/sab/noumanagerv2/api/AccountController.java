ebf28ba49f496a754bf96dec68e2a8c8976a3d37
Saad <saadabbou89@gmail.com>
2021-08-22 12:13:48 +0100

    cross origin

--- src/main/java/com/sab/noumanagerv2/api/AccountController.java
+++ src/main/java/com/sab/noumanagerv2/api/AccountController.java
@@ -7,12 +7,14 @@ import com.sab.noumanagerv2.entities.AppUser;
 import com.sab.noumanagerv2.service.AccountService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class AccountController {
